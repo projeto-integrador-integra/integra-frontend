beac22833b3e9f0656340354a59349bee9e77be5
Fernanda Oshiro <fernanda.sayuri@outlook.com>
2025-04-30 19:20:45 -0300

    feat: implement TagInput component and integrate it into CreateProjectForm for project tagging

---
+++ src/pages/empresa/components/CreateProjectForm.tsx
@@ -0,0 +1,83 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { TagInput } from '@/components/ui/tag-input'
+import { toaster } from '@/components/ui/toaster'
+import { apiErrorSchema } from '@/schema/error.schema'
+import { ProjectCreationSchema, ProjectCreationType } from '@/schema/project.schema'
+import { createProject } from '@/service/project'
+import { Box, Card, Heading, Textarea } from '@chakra-ui/react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Controller, useForm } from 'react-hook-form'
+
+export const CreateProjectForm = () => {
+  const {
+    register,
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: zodResolver(ProjectCreationSchema),
+  })
+
+  const onSubmit = async (data: ProjectCreationType) => {
+    try {
+      await createProject(data)
+      toaster.success({
+        title: 'Projeto criado com sucesso',
+        description: 'Seu projeto foi criado com sucesso.',
+      })
+      reset()
+    } catch (error) {
+      console.error('Erro ao criar projeto:', error)
+
+      const parsed = apiErrorSchema.safeParse(error)
+      const message = parsed.success
+        ? parsed.data.message
+        : 'Ocorreu um erro ao criar o projeto. Tente novamente mais tarde.'
+
+      toaster.error({
+        title: 'Erro ao criar projeto',
+        description: message,
+      })
+    }
+  }
+
+  return (
+    <Card.Root w="100%" variant="elevated" p="8">
+      <Heading mb="8">Conte-nos o que precisa realizar</Heading>
+      <Box as="form" onSubmit={handleSubmit(onSubmit)} display="flex" flexDir="column" gap="8">
+        <Input
+          label="Escolha um nome para o seu projeto"
+          placeholder="Por exemplo: crie um site para mim"
+          error={errors.name?.message}
+          {...register('name')}
+        />
+        <Input
+          as={Textarea}
+          rows={4}
+          label="Conte mais sobre o seu projeto"
+          placeholder="Descreva seu projeto aqui..."
+          error={errors.description?.message}
+          {...register('description')}
+        />
+        <Controller
+          name="tags"
+          control={control}
+          render={({ field }) => (
+            <TagInput
+              value={field.value}
+              setValue={field.onChange}
+              error={errors.tags?.message}
+              label="Digite as tags do seu projeto"
+            />
+          )}
+        />
+
+        <Button type="submit" ml="auto" mt="4" px="16" isLoading={isSubmitting}>
+          Enviar
+        </Button>
+      </Box>
+    </Card.Root>
+  )
+}
--- src/schema/project.schema.ts
+++ src/schema/project.schema.ts
@@ -1,41 +1,46 @@
-import { z } from 'zod'
 import { PROJECT_APPROVAL_STATUSES, PROJECT_STATUS } from '@/constants/project'
+import { z } from 'zod'
+import { UserSchema } from './user.schema'
 
 export const ProjectSchema = z.object({
-  id: z.string().uuid().optional(),
+  id: z.string().uuid({ message: 'ID inválido' }).optional(),
+
   name: z
-    .string()
-    .min(1, { message: 'O nome do projeto é obrigatório.' })
-    .max(50, { message: 'O nome do projeto deve ter no máximo 50 caracteres.' }),
-  description: z.string().max(300, { message: 'A descrição deve ter no máximo 300 caracteres.' }),
+    .string({ message: 'O nome do projeto deve ter no mínimo 3 caracteres' })
+    .min(3, { message: 'O nome do projeto deve ter no mínimo 3 caracteres' })
+    .max(50, { message: 'O nome do projeto deve ter no máximo 50 caracteres' }),
+
+  description: z
+    .string({ message: 'A descrição deve ter no mínimo 10 caracteres' })
+    .min(10, { message: 'A descrição deve ter no mínimo 10 caracteres' })
+    .max(300, { message: 'A descrição deve ter no máximo 300 caracteres' }),
+
   creatorId: z
     .string()
-    .uuid({ message: 'ID do criador inválido.' })
-    .nonempty({ message: 'O ID do criador é obrigatório.' }),
-  tags: z.string().array().optional(),
+    .uuid({ message: 'ID do criador inválido' })
+    .nonempty({ message: 'O ID do criador é obrigatório' }),
+
+  tags: z.array(z.string().min(1, { message: 'Tags não podem estar vazias' })).optional(),
+
   needsMentors: z.boolean().default(true),
   needsDevs: z.boolean().default(true),
+
   maxParticipants: z
     .number()
-    .min(1, { message: 'O número mínimo de participantes é 1.' })
-    .max(5, { message: 'O número máximo de participantes é 5.' })
+    .min(1, { message: 'É necessário pelo menos 1 participante' })
+    .max(5, { message: 'O máximo de participantes permitido é 5' })
     .default(3),
-  status: z
-    .enum(PROJECT_STATUS, {
-      errorMap: () => ({
-        message: 'Status do projeto inválido.',
-      }),
-    })
-    .default('active'),
+
+  status: z.enum(PROJECT_STATUS, { message: 'Status inválido' }).default('active'),
+
   approvalStatus: z
-    .enum(PROJECT_APPROVAL_STATUSES, {
-      errorMap: () => ({
-        message: 'Status de aprovação inválido.',
-      }),
-    })
+    .enum(PROJECT_APPROVAL_STATUSES, { message: 'Status de aprovação inválido' })
     .default('pending'),
+
   createdAt: z.date().optional(),
   updatedAt: z.date().optional(),
+
+  members: z.array(UserSchema).optional(),
 })
 
 export type ProjectType = z.infer<typeof ProjectSchema>
@@ -48,7 +53,10 @@ export const ProjectCreationSchema = ProjectSchema.omit({
   needsMentors: true,
   needsDevs: true,
   approvalStatus: true,
+  status: true,
+  members: true,
 })
+
 export type ProjectCreationType = z.infer<typeof ProjectCreationSchema>
 
 export const ProjectUpdateSchema = ProjectSchema.partial().omit({
